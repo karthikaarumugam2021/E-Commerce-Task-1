44a37255b56e9f012af94a7ff0ba1bb050d30b16
Karthika <karthikaarumugam2021@gmail.com>
2026-09-21 10:22:25 +0530

    Add files via upload

---
+++ sample task-1.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE ecommerce;
+CREATE TABLE customer (
+    Customer_ID INT PRIMARY KEY,
+    Customer_Name VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    Phone VARCHAR(15) NOT NULL UNIQUE,
+    Address VARCHAR(150) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    Registration_Date DATE NOT NULL
+);USE ecommerce;
+
+INSERT INTO customer
+(Customer_ID, Customer_Name, Email, Phone, Address, City, Registration_Date)
+VALUES
+(1, 'Dharshini', 'dharshini@gmail.com', '9876543210', 'Main Road', 'Karur', '2026-01-10'),
+(2, 'Priya', 'priya@gmail.com', '9876543211', 'Gandhi Nagar', 'Coimbatore', '2026-01-12'),
+(3, 'Anu', 'anu@gmail.com', '9876543212', 'Anna Nagar', 'Chennai', '2026-01-15'),
+(4, 'Kavya', 'kavya@gmail.com', '9876543213', 'MG Road', 'Madurai', '2026-01-18'),
+(5, 'Keerthana', 'keerthana@gmail.com', '9876543214', 'Bus Stand Road', 'Salem', '2026-01-20'),
+(6, 'saranya', 'saranya@gmail.com', '9876543215', 'Market Road', 'Trichy', '2026-01-22'),
+(7, 'Swetha', 'swetha@gmail.com', '9876543216', 'Temple Road', 'Erode', '2026-01-25'),
+(8, 'Divya', 'divya@gmail.com', '9876543217', 'Nehru Street', 'Tiruppur', '2026-01-28'),
+(9, 'Meena', 'meena@gmail.com', '9876543218', 'Lake Road', 'Thanjavur', '2026-02-01'),
+(10, 'Pavithra', 'pavithra@gmail.com', '9876543219', 'College Road', 'Namakkal', '2026-02-05');
+DESC customer;
+SELECT * FROM customer;
